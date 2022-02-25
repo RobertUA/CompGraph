b9b848b0d0025aaa959e7057c3a566ca1bf9d0ab
Роберт Шевцов <91878034+RobertUAggo@users.noreply.github.com>
2022-02-25 14:43:15 +0200

    Raycast, Plane

---
+++ DrawObjects/Plane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Plane : Drawable
+{
+    Vector position;
+    Vector normal;
+    public Plane(Vector position, Vector normal)
+    {
+        this.position = position;
+        this.normal = normal;
+    }
+    public override void Draw()
+    {
+        base.Draw();
+        Console.WriteLine("Obj class: " + GetType().ToString());
+    }
+}
