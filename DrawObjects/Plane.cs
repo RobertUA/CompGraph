3d0bf07abdd03746aa0d31ca7b7985a2cfa38f6d
MaksPetrukhnoPI <maxim.petrukhno@postindustria.com>
2022-06-08 00:26:56 +0200

    added plane intersection

--- DrawObjects/Plane.cs
+++ DrawObjects/Plane.cs
@@ -4,14 +4,22 @@ public class Plane : Drawable
 {
     Vector position;
     Vector normal;
+
     public Plane(Vector position, Vector normal)
     {
         this.position = position;
         this.normal = normal;
     }
+
+    public override Vector GetIntersection(Vector start, Vector direction)
+    {
+        float denom = Vector.Dot(normal, direction);
+        float t = Vector.Dot(position - start, normal) / denom;
+        return start + direction * t;
+    }
+
     public override void Draw()
     {
-        base.Draw();
         Console.WriteLine("Obj class: " + GetType().ToString());
     }
 }
