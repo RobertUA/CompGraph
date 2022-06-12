f0fac234206f3d65e3c2b5a80bd7fd123c91f568
Роберт Шевцов <91878034+RobertUAggo@users.noreply.github.com>
2022-06-12 18:33:04 +0300

    love

--- Utils/Scene.cs
+++ Utils/Scene.cs
@@ -14,15 +14,15 @@ class Scene
 
         //---- Setup
         Screen screen = new Screen(60, 30);
-        Camera camera = new Camera(new Vector(0, 0, -4f), Vector.forward, 50, 30, screen);
+        Camera camera = new Camera(new Vector(0, 0, -4f), Vector.forward, 40, 35, screen);
         //---- Drawables
-        /*Sphere sphere1 = new Sphere(new Vector(-1, 0, 0), 1);
+        Sphere sphere1 = new Sphere(new Vector(-1, 0.5f, 0), 1.25f);
         Drawables.Add(sphere1);
-        Sphere sphere2 = new Sphere(new Vector(1, 0, 0), 1);
-        Drawables.Add(sphere2);*/
-        Drawables.Add(new Triangle(new Vector(-2, -2, 0),
-                                            new Vector(2, -2, 0),
-                                            new Vector(0, 2, 0)));
+        Sphere sphere2 = new Sphere(new Vector(1, 0.5f, 0), 1.25f);
+        Drawables.Add(sphere2);
+        Drawables.Add(new Triangle(new Vector(-2, 0, 0),
+                                            new Vector(2, 0, 0),
+                                            new Vector(0, -2, 0)));
         //---- Start
         screen.Update();
     }
