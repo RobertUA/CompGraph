e534d812a9bbb35caf087836a776177ca3917dde
Роберт Шевцов <91878034+RobertUAggo@users.noreply.github.com>
2022-06-12 20:37:22 +0300

    Update HitInfo.cs

--- Utils/HitInfo.cs
+++ Utils/HitInfo.cs
@@ -1,11 +1,13 @@
 ﻿
 public class HitInfo
 {
+    public Drawable Drawable;
     public Vector Position;
     public Vector Normal;
-    public HitInfo(Vector hitPosition, Vector hitNormal)
+    public HitInfo(Vector hitPosition, Vector hitNormal, Drawable drawable)
     {
         Position = hitPosition;
         Normal = hitNormal;
+        Drawable = drawable;
     }
 }
